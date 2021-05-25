e45ff0ce1f6828e9eb760d3742a153fa229783b0
cuiwenkai <wkai.c@qq.com>
2021-05-25 17:07:08 +0800

    第一次测试提交

---
+++ Test1/src/HHH.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author: Kakajaya
+ * @Date: 2021/5/25-17:06
+ */
+public class HHH {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
